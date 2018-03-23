677de940e729276f40401d23cc3fdbb1adf55abe
Roland Willis <rolandwillisiv@gmail.com>
2018-03-23 11:34:22 -0700

    formated cards as panels and added draw method

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,31 +5,54 @@ var Card = function (n, hp, ap, cap, imgSrc){
     this.attackPower = ap;
     this.counterAP = cap;
     this.imageSource = imgSrc;
+    this.currentSlot = 0;
+    this.newSlot = 0;
     
-    // methods
-    this.drawCard = function(slotNum) {
+    // draws the card to a panel and handles clearing the columns
+    this.drawCard = function(slotNum, cleared) {
+        this.newSlot = slotNum;
 
-        var colSlot = $("#slot"+slotNum);
-        /*(colSlot.append('<div class="panel panel-default">');
-        colSlot.append('<div class="panel-heading"><h3 class="panel-title">' + this.name +'</h3></div>');
-        colSlot.append('<div class="panel-body">Image Goes Here</div>');
-        colSlot.append('<div class="panel-footer">HP:' + this.healthPoints +'</div>');
-        colSlot.append('</div>');*/
+        // Clears the previous slot the card was in
+        if (!(this.currentSlot === 0)){
+            var colSlot = $("#slot"+this.currentSlot);
+            colSlot.empty();
+        }
+        
+        // targets the new slot for the card to be inserted in
+        var colSlot = $("#slot"+this.newSlot);
 
-        colSlot.append('<div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title">'+this.name+'</h3></div><div class="panel-body"><img src="'+this.imageSource+'" class="img-responsive"></div><div class="panel-footer">HP:'+this.healthPoints+'</div></div>');
+        // clears the new slot if 1 is passed through the argument
+        if (cleared === 1) {
+            colSlot.empty();
+        }
+        
+        // Draws the card to a formated panel
+        colSlot.append(
+            '<div class="panel panel-primary">'+
+            '<div class="panel-heading">'+
+            '<h3 class="panel-title">'+this.name+'</h3>'+
+            '</div>'+
+            '<div class="panel-body">'+
+            '<img src="'+this.imageSource+'" class="img-responsive">'+
+            '</div>'+
+            '<div class="panel-footer text-center"><span class="badge">HP:'+this.healthPoints+'</span><span class="badge">AP:'+this.attackPower+'</span><span class="badge">CAP:'+this.counterAP+'</span></div>'+
+            '</div>');
+
+        this.currentSlot = this.newSlot;
+            
     }
     
 };
 
-var obiWan = new Card("Obi Wan", 10, 15, 20, "assets/images/luke.jpg");
-var darthMaul = new Card("Darth Maul", 20, 10, 5, "assets/images/luke.jpg");
-var palpatine = new Card("Palpatine", 15, 30, 12, "assets/images/luke.jpg");
+var obiWan = new Card("Obi Wan", 10, 15, 20, "assets/images/obiwan.jpg");
+var darthMaul = new Card("Darth Maul", 20, 10, 5, "assets/images/darthmaul.jpeg");
+var darthSidious = new Card("Palpatine", 15, 30, 12, "assets/images/darthsidious.jpg");
 var luke = new Card("Luke", 5, 10, 10, "assets/images/luke.jpg");
 
-luke.drawCard(1);
-obiWan.drawCard(2);
-palpatine.drawCard(3);
-darthMaul.drawCard(4);
+luke.drawCard(1, 1);
+obiWan.drawCard(2, 1);
+darthSidious.drawCard(3, 1);
+darthMaul.drawCard(4, 1);
 
 
 
